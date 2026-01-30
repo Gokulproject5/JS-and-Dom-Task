25754fae55244ba9bc6db43c871f44c93d0573ea
Gokulproject5 <gokulakrishnanait2022@jkkn.ac.in>
2026-01-30 20:08:05 +0530

    Feat:Update New  Project: Website Quote

---
+++ tasks/quote/dummy.js
@@ -0,0 +1,28 @@
+  <main class="fade-up  mt-3 h-1/2 md:min-h-screen ">
+        
+         <div class="px-2 bg-white ">
+           <!-- LAnding page  -->
+          <div class="bg-[url(/bg-hh.jpg)] bg-auto md:bg-cover bg-center  z-10  rounded-3xl">
+             <!-- main page Wrapper -->
+               <div class="px-20 rounded-xl  h-dvh  flex items-center  bg-linear-to-r from-black/70 backdrop-brightness-150 backdrop-blur to-black/50 ">
+                   <div class="flex flex-col flex-1 items-center justify-center">
+                    <div class=" space-y-5  ">
+                     <div class=" space-y-4 font-large max-w-sm mx-auto">
+                       <h1 class="text-platinum text-6xl  ">Is Your Brand</h1>
+                       </div>
+                       <div class=" space-y-4 font-large mx-auto ">
+                  <h1 class="text-platinum text-6xl">Attracting Prospects?</h1>
+                     </div>
+                   <div class="flex">
+                        <p class="text-lg md:text-3xl text-yellow-500 mx-auto ">Let us discuss</p>
+                   </div>
+                  
+                   <div class="bg-rose-400 text-platinum py-2 px-10 text-md md:text-2xl rounded-3xl mx-auto w-fit space-x-5 ">
+                       <h1 class="mx-auto flex items-center"><svg xmlns="http://www.w3.org/2000/svg" height="35px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M263.72-96Q234-96 213-117.15T192-168v-456q0-29.7 21.15-50.85Q234.3-696 264-696h72v-16q0-60 40.5-106T480-864q60 0 102 42t42 102v24h72q29.7 0 50.85 21.15Q768-653.7 768-624v456q0 29.7-21.16 50.85Q725.68-96 695.96-96H263.72Zm.28-72h432v-456h-72v60q0 15.3-10.29 25.65Q603.42-528 588.21-528t-25.71-10.35Q552-548.7 552-564v-60H408v60q0 15.3-10.29 25.65Q387.42-528 372.21-528t-25.71-10.35Q336-548.7 336-564v-60h-72v456Zm144-528h144v-24q0-29.7-21.21-50.85-21.21-21.15-51-21.15T429-770.85Q408-749.7 408-720v24ZM264-168v-456 456Z"/></svg> Web Based Service</h1>
+                  </div>
+                   </div>
+                  </div>
+               </div>
+             </div>
+          </div>
+      </main>
